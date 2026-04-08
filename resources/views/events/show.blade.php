1ad0ccf70d1ac2878e12e715d8e4c6057620c924
Elian <elianperezrom@gmail.com>
2026-04-08 16:33:25 -0600

    fix: Update event title display in camera and show views

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Evento</title>
+    <title> {{ $event->name ? $event->name : 'Evento' }}</title>
     <style>
         body {
             margin: 0;
